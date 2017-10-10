fd150d67ded7653023aa8dabff9ae85cac96c07e
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-10 16:43:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif speak.upcase == speak
+    puts "NO, NOT SINCE 1938!"
+  else speak
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
