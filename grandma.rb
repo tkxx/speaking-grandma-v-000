3701b8eb905a01b1483dde33407f007573319d4b
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-10 16:44:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif speak.upcase == speak
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else speak
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
